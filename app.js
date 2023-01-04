5c5e460a295a4ba039ca1a5688c8899b76c4e240
Gowtham Jiripurapu <gowthamnag04@gmail.com>
2023-01-04 17:25:58 +0530

    updated .env vars

--- app.js
+++ app.js
@@ -10,10 +10,11 @@ const e = require("express");
 const Posts = require('./user_model')
 require('dotenv').config()
 ////////////////////////////////////////////////////////////////////////////////
-const url = process.env.url
+const Port = process.env.port
+const url = 'mongodb+srv://gotham:G0thamXmong0@journal.4ok3oge.mongodb.net/journal?retryWrites=true&w=majority' //process.env.DB_conn
 mongoose.set('strictQuery', false)
 try{mongoose.connect(url,{useNewUrlParser : true})}
-catch(err){console.log(err)}
+catch(err){console.log("connectoin failed")}
 /////////////////////////////////////////////////////////////////////////////////
 const homeStartingContent ="Home content Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -122,6 +123,6 @@ app.get('/home/posts/:posts', (req, res) => {
 ///////////////////////////////////////////////////////////
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(Port, function() {
+  console.log("Server started on port "+Port);
 })
